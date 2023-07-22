7a66dab7500e7cc6c3aedbce8b92393e42f3b973
Heinzoo <enzoquiomaster.com>
2023-07-22 19:58:15 -0400

    users and publications

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  resources :publications, except: [ :index]
+  get '/publications', to: 'publications#index', as: 'user_root'
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "publications#index"
 end
