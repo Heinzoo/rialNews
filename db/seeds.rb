2eba98b01210f93418e02fe7cf04d97984ccb371
Heinzoo <enzoquiomaster.com>
2023-07-23 13:26:20 -0400

    vistas con boostrap

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create!([
+  { email: 'enzoquiomaster@gmail.com',
+    password: '123456'
+  }
+])
+9.times do
+  Publication.create!([
+    { image: 'https://picsum.photos/id/' + rand(1..100).to_s + '/200',
+      title: Faker::Lorem.sentence(word_count: 5),
+      description: Faker::Lorem.paragraphs(number:2),
+      user_id: 1
+    }
+  ])
+end
