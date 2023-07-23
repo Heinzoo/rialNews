16b0fb0c9e792d1a02c289bf07c852fb1e1d784a
Heinzoo <enzoquiomaster.com>
2023-07-22 23:41:01 -0400

    comentarios

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users
-  resources :publications, except: [ :index]
+  resources :publications, except: [:index] do
+    resources :comments
+  end
   get '/publications', to: 'publications#index', as: 'user_root'
 
   # Defines the root path route ("/")
